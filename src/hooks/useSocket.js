9e0704b3a2e4a07f546556aef9106fcab230ad21
Danna Olmedo <DannaOlmedo>
2026-02-14 11:46:08 -0600

    Cambio de url a dinamica

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io(process.env.BACK_URL || "http://localhost:4000");
 
 export const useSocket = (setReportes) => {
   useEffect(() => {
--- src/services/reporte.service.js
+++ src/services/reporte.service.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:4000/api/reportes";
+const DEV_BACK_URL = "http://localhost:4000";
+const API_URL = (process.env.BACK_URL || DEV_BACK_URL) + "/api/reportes";
 
 export const obtenerReportes = async () => {
   const res = await fetch(API_URL);
