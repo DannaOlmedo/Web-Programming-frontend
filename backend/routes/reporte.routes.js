3324afe5ef1ca34b85ea109e629bfc8a03e3f6ea
Danna Olmedo <DannaOlmedo>
2026-02-13 18:43:11 -0600

    try deploy only front end

--- backend/routes/reporte.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const reporteController = require("../controllers/reporte.controller");
-
-router.get("/", reporteController.obtenerReportes);
-router.post("/", reporteController.crearReporte);
-
-module.exports = router;
